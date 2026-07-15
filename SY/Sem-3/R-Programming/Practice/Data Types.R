ba0ddf3251dfe61e01581bcd3cbc79329b932b2e
Sadiya Mulani <sadiyamulani03@gmail.com>
2026-07-15 16:23:28 +0530

    Initial commit

---
+++ SY/Sem-3/R-Programming/Practice/Data Types.R
@@ -0,0 +1,56 @@
+#double
+a <- 8
+typeof(a)
+
+b <- 4.5
+typeof(b)
+
+#integer
+c <- 8L
+typeof(c)
+
+#character
+answer = "4>5"
+answer
+typeof(answer)
+is.character(answer)
+
+#complex data type
+z8 <- 3 + 4i
+typeof(z8)
+class(z8)
+
+z1 <- 5i
+
+z2 <- complex(real = 2, imaginary = 6)
+
+#raw
+d <- as.raw()
+
+class(d)
+typeof(d)
+
+#Type casting (conversion)
+name <- "ABC"
+as.numeric(name)
+
+#sort
+e = c(7,6,8)
+e
+sort(e)
+typeof(abc)
+
+#summary
+summary(e)
+
+studentnames = c("Sadiya","Rachana","Alicia","Shahin")
+nchar(studentnames)
+sort(studentnames)
+
+#factor variable
+gender = c("M","F","M","F","F")
+class(gender)
+typeof(gender)
+fgen = as.factor(gender)
+fgen
+summary(fgen)
---
+++ SY/Sem-3/R-Programming/Practice/Functions.R
@@ -0,0 +1,31 @@
+abc = c(4,7,9,3)
+meanofabc = sum (abc)/length(abc)
+meanofabc
+
+mean(abc)
+
+# to remove from environment
+rm (meanofabc)
+
+values = c(98, 4, 68, 62)
+mymean = function (y){sum(y)/length(y)}
+mymean(values)
+
+p = c(5,9,5,3,4,55,35,568,35,7)
+staddevia = function(s){}
+
+triArea = function(base, height){
+  triangle_base = as.numeric(readline("Enter base value:"))
+  triangle_height = as.numeric(readline("Enter height value:"))
+  return(0.5*triangle_base*triangle_height)}
+triArea()
+
+triangle_base = as.numeric(readline("Enter base value:"))
+triangle_height = as.numeric(readline("Enter height value:"))
+result = triArea(triangle_base, triangle_height)
+result
+
+L = 3
+B = 5
+areaofrec = function(l,b){return(l*b)}
+areaofrec(L,B)
