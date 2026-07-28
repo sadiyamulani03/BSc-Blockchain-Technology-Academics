0ec7133497a5e279aab35d7e769387a40fbe4d3d
sadiyamulani03 <sadiyamulani03@gmail.com>
2026-07-28 11:27:37 -0700

    Initial commit

--- SY/Sem-3/R-Programming/Practice/filehandling.R
+++ SY/Sem-3/R-Programming/Practice/filehandling.R
@@ -1,4 +1,7 @@
+#A data frame in R is a fundamental, two-dimensional data structure designed to store tabular data where columns can contain different data types
 data = read.csv("D:/Uni/SY/Sem-3/R-Programming/employee/acbb2271e66c10a5b73aacf82ca82784-e38afe62e088394d61ed30884dd50a6826eee0a8/employees.csv")
+#24
+newemp1 = data[2,2]
 
 #coping dataset in another var 
 data_1 = data
@@ -16,7 +19,7 @@ str(data_1)
 colnames(data_1)
 
 #21
-#Subset of dataset 
+#Subset of dataset is a filtered dataset.
 subset(data_1,EMPLOYEE_ID>25)
 
 # replace values in dataset use 
@@ -58,8 +61,8 @@ DF = data.frame(
 DF
 
 #filter values
-subset(DF, is.na(a))
-subset(DF, is.na(b))
+subset(DF, !is.na(a))
+subset(DF, !is.na(b))
 #omit(removes columns according to condition)
 na.omit(DF)
 
@@ -92,6 +95,8 @@ DF_1 = data.frame(
   sal = c(data_1$SALARY)
 )
 
+# a data point that significantly deviates from other observations in a dataset, 
+#often due to variability, measurement errors, or rare events
 #exclude a Outlier
 library(UsingR)
 x = babies$dwt
@@ -99,7 +104,26 @@ head(x)
 #range of dataset 
 x[4:8]
 summary(x)
-#replace 999 outlier
+#replace 999 Outlier
 x[x==999]=NA
 range(x, na.rm=T)
 dim(x)
+
+#24
+newemp1 = data[2,2]
+newemp1
+exmpdata = data
+exmpdata[is.na(exmpdata)] = 0
+exmpdata
+summary(exmpdata)
+
+#existing dataset
+library(UsingR)
+babies
+summary(babies)
+head(babies)
+colnames(babies)
+dim(babies)
+str(babies)
+x = babies$dwt
+x[4:8]
--- SY/Sem-3/R-Programming/Practice/vectors.R
+++ SY/Sem-3/R-Programming/Practice/vectors.R
@@ -1,3 +1,5 @@
+#vector is a collection of similar type of data 
+
 nums1=c(3,6,9,12)
 nums2=c(5,2,8,1)
 add = nums1 + nums2
